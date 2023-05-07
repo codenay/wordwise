bde4a69edee28b976f2b6c48c2cd30f3df9a455e
“codenay” <koffieawuku@gmail.com>
2023-05-07 18:59:52 +0000

    minor changes:footer bg color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div>
         <footer>
-            <div className='fixed bottom-0 w-full mt-20'>
+            <div className='ft fixed bottom-0 w-full mt-20'>
                 <div className="mx-auto  flex max-w-6xl items-center justify-between pt-4 pb-2 px-6 lg:px-8 border-t border-1 border-slate-300" aria-label="Global">
                     <div>
                         <p className='text-slate-400 font-light'>Designed and built by <span className='text-slate-800 font-normal' ><a target="_blank" href="https://github.com/codenay">KoFi</a></span> </p>
