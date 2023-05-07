4b84485adaf92b8d27fc7e9ac081bf8b365421ed
“codenay” <koffieawuku@gmail.com>
2023-05-07 19:04:33 +0000

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
     <div>
         <footer>
             <div className='ft fixed bottom-0 w-full mt-20'>
-                <div className="mx-auto  flex max-w-6xl items-center justify-between pt-4 pb-2 px-6 lg:px-8 border-t border-1 border-slate-300" aria-label="Global">
+                <div className="mx-auto  flex max-w-6xl items-center justify-between pt-2 pb-2 px-6 lg:px-8 border-t border-1 border-slate-300" aria-label="Global">
                     <div>
-                        <p className='text-slate-400 font-light'>Designed and built by <span className='text-slate-800 font-normal' ><a target="_blank" href="https://github.com/codenay">KoFi</a></span> </p>
+                        <p className='text-slate-400 font-light text-sm'>Designed and built by <span className='text-slate-800 font-normal' ><a target="_blank" href="https://github.com/codenay">KoFi</a></span> </p>
                     </div>
                     <div className='w-28 hidden md:flex  justify-between'>
                         <a href="https://twitter.com/kofi_ui"><img src={twitterIcon}  className='p-2 rounded-md hover:bg-slate-200 duration-150 ease-in-out'/></a>
